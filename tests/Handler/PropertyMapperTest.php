d7be45385299ae9330732a283ec70b41b8eedf50
Danny van der Sluijs <danny.vandersluijs@icloud.com>
2020-04-02 22:16:33 +0200

    Allow for multiple clover reports

---
+++ tests/Handler/PropertyMapperTest.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types=1);
+
+namespace JsonMapper\Tests\Handler;
+
+use JsonMapper\Handler\PropertyMapper;
+use JsonMapper\JsonMapperInterface;
+use JsonMapper\ValueObjects\PropertyMap;
+use JsonMapper\Wrapper\ObjectWrapper;
+use PHPUnit\Framework\TestCase;
+
+class PropertyMapperTest extends TestCase
+{
+    /**
+     * @covers \JsonMapper\Handler\PropertyMapper
+     */
+    public function testAdditionalJsonIsIgnored(): void
+    {
+        $mapper = new PropertyMapper();
+        $json = (object) ['file' => __FILE__];
+        $object = new \stdClass();
+
+        $mapper->__invoke($json, new ObjectWrapper($object), new PropertyMap(), $this->createMock(JsonMapperInterface::class));
+
+        self::assertEquals(new \stdClass(), $object);
+    }
+
+}
