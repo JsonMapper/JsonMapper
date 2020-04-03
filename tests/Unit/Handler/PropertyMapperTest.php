29f48aa882a3d979ddf4f0103972fbde0e3ab436
Danny van der Sluijs <danny.vandersluijs@icloud.com>
2020-04-03 16:41:22 +0200

    Add more tests on property mapper

--- tests/Unit/Handler/PropertyMapperTest.php
+++ tests/Unit/Handler/PropertyMapperTest.php
@@ -2,6 +2,8 @@
 
 namespace JsonMapper\Tests\Unit\Handler;
 
+use JsonMapper\Builders\PropertyBuilder;
+use JsonMapper\Enums\Visibility;
 use JsonMapper\Handler\PropertyMapper;
 use JsonMapper\JsonMapperInterface;
 use JsonMapper\ValueObjects\PropertyMap;
@@ -22,6 +24,51 @@ class PropertyMapperTest extends TestCase
         $mapper->__invoke($json, new ObjectWrapper($object), new PropertyMap(), $this->createMock(JsonMapperInterface::class));
 
         self::assertEquals(new \stdClass(), $object);
+    }
+
+    /**
+     * @covers \JsonMapper\Handler\PropertyMapper
+     */
+    public function testPublicScalarValueIsSet(): void
+    {
+        $fileProperty = PropertyBuilder::new()
+            ->setName('file')
+            ->setType('string')
+            ->setIsNullable(false)
+            ->setVisibility(Visibility::PUBLIC())
+            ->build();
+        $propertyMap = new PropertyMap();
+        $propertyMap->addProperty($fileProperty);
+        $json = (object) ['file' => __FILE__];
+        $object = new \stdClass();
+        $mapper = new PropertyMapper();
+
+        $mapper->__invoke($json, new ObjectWrapper($object), $propertyMap, $this->createMock(JsonMapperInterface::class));
+
+        self::assertEquals(__FILE__, $object->file);
+    }
+
+    /**
+     * @covers \JsonMapper\Handler\PropertyMapper
+     */
+    public function testPublicBuiltinClassIsSet(): void
+    {
+        $fileProperty = PropertyBuilder::new()
+            ->setName('createdAt')
+            ->setType(\DateTimeImmutable::class)
+            ->setIsNullable(false)
+            ->setVisibility(Visibility::PUBLIC())
+            ->build();
+        $now = new \DateTimeImmutable();
+        $propertyMap = new PropertyMap();
+        $propertyMap->addProperty($fileProperty);
+        $json = (object) ['createdAt' => $now->format('Y-m-d\TH:i:s.uP')];
+        $object = new \stdClass();
+        $mapper = new PropertyMapper();
+
+        $mapper->__invoke($json, new ObjectWrapper($object), $propertyMap, $this->createMock(JsonMapperInterface::class));
+
+        self::assertEquals($now, $object->createdAt);
     }
 
 }
