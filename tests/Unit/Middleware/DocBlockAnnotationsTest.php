236ac7ac2206497644a31b06b6ac8728d93e39c5
Danny van der Sluijs <danny.vandersluijs@icloud.com>
2020-04-07 22:27:48 +0200

    Add TypedProperties unit test

---
+++ tests/Unit/Middleware/DocBlockAnnotationsTest.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JsonMapper\Tests\Middleware;
+
+use JsonMapper\Enums\Visibility;
+use JsonMapper\JsonMapperInterface;
+use JsonMapper\Middleware\DocBlockAnnotations;
+use JsonMapper\Tests\Implementation\SimpleObject;
+use JsonMapper\ValueObjects\PropertyMap;
+use JsonMapper\Wrapper\ObjectWrapper;
+use PHPUnit\Framework\TestCase;
+
+class DocBlockAnnotationsTest extends TestCase
+{
+    /**
+     * @covers \JsonMapper\Middleware\DocBlockAnnotations
+     */
+    public function testHandlesMethod(): void
+    {
+        $middleware = new DocBlockAnnotations();
+        $object = new SimpleObject();
+        $propertyMap = new PropertyMap();
+        $jsonMapper = $this->createMock(JsonMapperInterface::class);
+
+        $middleware->handle(new \stdClass(), new ObjectWrapper($object), $propertyMap, $jsonMapper);
+
+        self::assertTrue($propertyMap->hasProperty('name'));
+        self::assertEquals('string', $propertyMap->getProperty('name')->getType());
+        self::assertEquals(Visibility::PRIVATE(), $propertyMap->getProperty('name')->getVisibility());
+        self::assertFalse($propertyMap->getProperty('name')->isNullable());
+    }
+}
