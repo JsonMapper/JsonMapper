49bb7cc70d94031a03fa6f16e2066c55855f7369
Danny van der Sluijs <danny.vandersluijs@icloud.com>
2020-04-13 13:55:30 +0200

    Add case conversion middleware; Resolves #4

---
+++ src/Enums/PropertyAccessorType.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JsonMapper\Enums;
+
+/**
+ * @method static PropertyAccessorType VIEW()
+ * @method static PropertyAccessorType EDIT()
+ */
+class PropertyAccessorType
+{
+    private const VIEW = 'view';
+    private const EDIT = 'edit';
+}
