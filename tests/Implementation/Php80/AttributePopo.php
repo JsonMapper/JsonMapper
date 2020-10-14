e447078ff1c5073c8f2db76ced897f1341593a4a
Danny van der Sluijs <danny.vandersluijs@icloud.com>
2020-10-14 21:56:11 +0200

    Add Attributes middleware; Small tweaks for PHP8.0 builds

---
+++ tests/Implementation/Php80/AttributePopo.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JsonMapper\Tests\Implementation\Php80;
+
+use JsonMapper\Middleware\Attributes\MapFrom;
+
+class AttributePopo
+{
+    #[MapFrom("Identifier")]
+    public int $id;
+    #[MapFrom("UserName")]
+    public string $name;
+}
