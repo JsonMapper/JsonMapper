26c35ce716dfb98edbdc4ee74a04cdfaa4a03300
Danny van der Sluijs <danny.vandersluijs@icloud.com>
2021-02-04 09:50:54 +0100

    Add Rename mapping (#75)
    * Add Rename mapping
    
    * Correct code style

--- tests/Integration/JsonMapperTest.php
+++ tests/Integration/JsonMapperTest.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace JsonMapper\Tests\Integration;
 
 use JsonMapper\JsonMapperFactory;
+use JsonMapper\Middleware\Rename\Rename;
 use JsonMapper\Tests\Implementation\ComplexObject;
+use JsonMapper\Tests\Implementation\Models\User;
 use JsonMapper\Tests\Implementation\Popo;
 use JsonMapper\Tests\Implementation\Php74;
 use JsonMapper\Tests\Implementation\SimpleObject;
@@ -456,6 +458,44 @@ class JsonMapperTest extends TestCase
         // Assert
         self::assertEquals($object->getPopos()[0]->name, $json->popos[0]->name);
         self::assertEquals($object->getPopos()[1]->name, $json->popos[1]->name);
+    }
+
+    public function testItCanRenameJsonProperties(): void
+    {
+        // Arrange
+        $rename = new Rename();
+        $rename->addMapping(User::class, 'Full-Name', 'name');
+        $rename->addMapping(User::class, 'Identifier', 'id');
+        $mapper = (new JsonMapperFactory())->bestFit();
+        $mapper->unshift($rename);
+        $object = new User();
+        $json = (object) ['Full-Name' => 'John Doe', 'Identifier' => '42'];
+
+        // Act
+        $mapper->mapObject($json, $object);
+
+        // Assert
+        self::assertEquals('John Doe', $object->getName());
+        self::assertEquals(42, $object->getId());
+    }
+
+    public function testItCanRenameJsonPropertiesOnNestedObjects(): void
+    {
+        // Arrange
+        $rename = new Rename();
+        $rename->addMapping(SimpleObject::class, 'FULL-NAME', 'name');
+        $rename->addMapping(ComplexObject::class, 'sub', 'children');
+        $mapper = (new JsonMapperFactory())->bestFit();
+        $mapper->unshift($rename);
+        $object = new ComplexObject();
+        $json = (object) ['sub' => [(object) ['FULL-NAME' => 'John Doe'], (object) ['FULL-NAME' => 'Jane Doe']]];
+
+        // Act
+        $mapper->mapObject($json, $object);
+
+        // Assert
+        self::assertCount(2, $object->getChildren());
+        self::assertEquals([new SimpleObject('John Doe'), new SimpleObject('Jane Doe')], $object->getChildren());
     }
 
     public function scalarValueDataTypes(): array
