510a94f5c2657cb85645d7741f7457f1809a0f6c
Danny van der Sluijs <danny.vandersluijs@icloud.com>
2021-03-30 21:12:33 +0200

    apply psr-12 code style

--- src/Exception/BuilderException.php
+++ src/Exception/BuilderException.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace JsonMapper\Exception;
 
-
 class BuilderException extends \Exception
 {
     public static function invalidJsonMapperClassName(string $className): self
