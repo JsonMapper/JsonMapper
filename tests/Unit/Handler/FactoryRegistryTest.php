d1fd7096a2b860fb1a113c2883641cd8e8a655b2
Danny van der Sluijs <danny.vandersluijs@icloud.com>
2021-05-12 21:18:42 +0200

    Add intial support for mapping interfaces and abstract classes (#84)
    * Add intial support for inheritance
    
    * Add php version annotation to test
    
    * Improve test to allow on lower PHP versions
    
    * Remove typed properties middleware from test to pass on >=PHP7.3
    
    * Cleanup Factory Registry and related classes
    
    * Add unit test for mapping to interface
    
    * Refactor to nonInstantiableTypeResolver
    
    * Add feature test
    
    * Correct variable name
    
    * Extract method for non instantiable types
    
    * Cleanup tests
    
    * Cleanup imports
    
    * Correct code style; Replace inline factory with object instance
    
    * Add changelog entry

--- tests/Unit/Handler/FactoryRegistryTest.php
+++ tests/Unit/Handler/FactoryRegistryTest.php
@@ -2,21 +2,97 @@
 
 declare(strict_types=1);
 
-namespace JsonMapper\Tests\Handler;
+namespace JsonMapper\Tests\Unit\Handler;
 
 use JsonMapper\Exception\ClassFactoryException;
-use JsonMapper\Handler\ClassFactoryRegistry;
+use JsonMapper\Handler\FactoryRegistry;
 use PHPUnit\Framework\TestCase;
 
-class ClassFactoryRegistryTest extends TestCase
+class FactoryRegistryTest extends TestCase
 {
     /**
-     * @covers \JsonMapper\Handler\ClassFactoryRegistry
+     * @covers \JsonMapper\Handler\FactoryRegistry
      */
-    public function testLoadNativePhpClassFactoriesAddsFactoriesForNativeClasses(): void
+    public function testAddFactoryAddsFactory(): void
     {
-        $classFactoryRegistry = new ClassFactoryRegistry();
-        $classFactoryRegistry->loadNativePhpClassFactories();
+        $classFactoryRegistry = new FactoryRegistry();
+        $classFactoryRegistry->addFactory(__CLASS__, static function () {
+        });
+
+        self::assertTrue($classFactoryRegistry->hasFactory(__CLASS__));
+    }
+
+    /**
+     * @covers \JsonMapper\Handler\FactoryRegistry
+     */
+    public function testHasFactoryReturnsFalseWhenNoFactoryRegistered(): void
+    {
+        $classFactoryRegistry = new FactoryRegistry();
+
+        self::assertFalse($classFactoryRegistry->hasFactory(__CLASS__));
+    }
+
+    /**
+     * @covers \JsonMapper\Handler\FactoryRegistry
+     */
+    public function testAddFactoryThrowsExceptionWhenDuplicateClassNameIsAdded(): void
+    {
+        $classFactoryRegistry = new FactoryRegistry();
+        $classFactoryRegistry->addFactory(__CLASS__, static function () {
+        });
+
+        $this->expectExceptionObject(ClassFactoryException::forDuplicateClassname(__CLASS__));
+
+        $classFactoryRegistry->addFactory(__CLASS__, static function () {
+        });
+    }
+
+    /**
+     * @covers \JsonMapper\Handler\FactoryRegistry
+     */
+    public function testCreateReturnsValueFromCallable(): void
+    {
+        $classFactoryRegistry = new FactoryRegistry();
+        $object = new \stdClass();
+        $classFactoryRegistry->addFactory(__CLASS__, static function () use ($object) {
+            return $object;
+        });
+
+        self::assertSame($object, $classFactoryRegistry->create(__CLASS__, new \stdClass()));
+    }
+
+    /**
+     * @covers \JsonMapper\Handler\FactoryRegistry
+     */
+    public function testCreateCanHandleLeadingSlash(): void
+    {
+        $classFactoryRegistry = new FactoryRegistry();
+        $object = new \stdClass();
+        $classFactoryRegistry->addFactory(\DateTimeImmutable::class, static function () use ($object) {
+            return $object;
+        });
+
+        self::assertSame($object, $classFactoryRegistry->create('\DateTimeImmutable', new \stdClass()));
+    }
+
+    /**
+     * @covers \JsonMapper\Handler\FactoryRegistry
+     */
+    public function testCreateThrowsExceptionForMissingFactory(): void
+    {
+        $classFactoryRegistry = new FactoryRegistry();
+
+        $this->expectExceptionObject(ClassFactoryException::forMissingClassname(__CLASS__));
+
+        $classFactoryRegistry->create(__CLASS__, new \stdClass());
+    }
+
+    /**
+     * @covers \JsonMapper\Handler\FactoryRegistry
+     */
+    public function testWithNativePhpClassesAddedAddsFactoriesForNativeClasses(): void
+    {
+        $classFactoryRegistry = FactoryRegistry::WithNativePhpClassesAdded();
 
         self::assertTrue($classFactoryRegistry->hasFactory(\DateTime::class));
         self::assertTrue($classFactoryRegistry->hasFactory(\DateTimeImmutable::class));
@@ -30,82 +106,5 @@ class ClassFactoryRegistryTest extends TestCase
             (object) ['one' => 1, 'two' => 2],
             $classFactoryRegistry->create(\stdClass::class, ['one' => 1, 'two' => 2])
         );
-    }
-
-    /**
-     * @covers \JsonMapper\Handler\ClassFactoryRegistry
-     */
-    public function testAddFactoryAddsFactory(): void
-    {
-        $classFactoryRegistry = new ClassFactoryRegistry();
-        $classFactoryRegistry->addFactory(__CLASS__, static function () {
-        });
-
-        self::assertTrue($classFactoryRegistry->hasFactory(__CLASS__));
-    }
-
-    /**
-     * @covers \JsonMapper\Handler\ClassFactoryRegistry
-     */
-    public function testHasFactoryReturnsFalseWhenNoFactoryRegistered(): void
-    {
-        $classFactoryRegistry = new ClassFactoryRegistry();
-
-        self::assertFalse($classFactoryRegistry->hasFactory(__CLASS__));
-    }
-
-    /**
-     * @covers \JsonMapper\Handler\ClassFactoryRegistry
-     */
-    public function testAddFactoryThrowsExceptionWhenDuplicateClassNameIsAdded(): void
-    {
-        $classFactoryRegistry = new ClassFactoryRegistry();
-        $classFactoryRegistry->addFactory(__CLASS__, static function () {
-        });
-
-        $this->expectExceptionObject(ClassFactoryException::forDuplicateClassname(__CLASS__));
-
-        $classFactoryRegistry->addFactory(__CLASS__, static function () {
-        });
-    }
-
-    /**
-     * @covers \JsonMapper\Handler\ClassFactoryRegistry
-     */
-    public function testCreateReturnsValueFromCallable(): void
-    {
-        $classFactoryRegistry = new ClassFactoryRegistry();
-        $object = new \stdClass();
-        $classFactoryRegistry->addFactory(__CLASS__, static function () use ($object) {
-            return $object;
-        });
-
-        self::assertSame($object, $classFactoryRegistry->create(__CLASS__, new \stdClass()));
-    }
-
-    /**
-     * @covers \JsonMapper\Handler\ClassFactoryRegistry
-     */
-    public function testCreateCanHandleLeadingSlash(): void
-    {
-        $classFactoryRegistry = new ClassFactoryRegistry();
-        $object = new \stdClass();
-        $classFactoryRegistry->addFactory(\DateTimeImmutable::class, static function () use ($object) {
-            return $object;
-        });
-
-        self::assertSame($object, $classFactoryRegistry->create('\DateTimeImmutable', new \stdClass()));
-    }
-
-    /**
-     * @covers \JsonMapper\Handler\ClassFactoryRegistry
-     */
-    public function testCreateThrowsExceptionForMissingFactory(): void
-    {
-        $classFactoryRegistry = new ClassFactoryRegistry();
-
-        $this->expectExceptionObject(ClassFactoryException::forMissingClassname(__CLASS__));
-
-        $classFactoryRegistry->create(__CLASS__, new \stdClass());
     }
 }
