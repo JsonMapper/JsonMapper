a8c14396ff7923cd258d9c65075d7325727f4ef5
Danny van der Sluijs <danny.vandersluijs@icloud.com>
2021-10-05 21:17:35 +0200

    Correct styling

--- tests/Implementation/Php81/Status.php
+++ tests/Implementation/Php81/Status.php
@@ -6,7 +6,7 @@ namespace JsonMapper\Tests\Implementation\Php81;
 
 enum Status: string
 {
-case DRAFT = 'draft';
-case PUBLISHED = 'published';
-case ARCHIVED = 'archived';
-    }
+    case DRAFT = 'draft';
+    case PUBLISHED = 'published';
+    case ARCHIVED = 'archived';
+}
