79b130852b54ca3ab1a8b73529aaa95de886a41c
Danny van der Sluijs <danny.vandersluijs@icloud.com>
2022-10-25 15:16:41 +0200

    test: Add test cases for DefaultFactory

---
+++ tests/Unit/Middleware/Constructor/DefaultFactoryTest.php
@@ -0,0 +1,158 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JsonMapper\Tests\Unit\Middleware\Constructor;
+
+use JsonMapper\Helpers\ScalarCaster;
+use JsonMapper\JsonMapperInterface;
+use JsonMapper\Middleware\Constructor\DefaultFactory;
+use PHPUnit\Framework\TestCase;
+
+class DefaultFactoryTest extends TestCase
+{
+    /**
+     * @covers \JsonMapper\Middleware\Constructor\DefaultFactory
+     */
+    public function testDefaultFactoryCanHandleObjectWithConstructorWithoutParameters(): void
+    {
+        $class = new class {
+            public function __construct()
+            {
+            }
+        };
+        $sut = new DefaultFactory(
+            get_class($class),
+            (new \ReflectionClass($class))->getConstructor(),
+            $this->createMock(JsonMapperInterface::class),
+            new ScalarCaster()
+        );
+
+        $result = $sut->__invoke(new \stdClass());
+
+        self::assertInstanceOf(get_class($class), $result);
+    }
+
+    /**
+     * @covers \JsonMapper\Middleware\Constructor\DefaultFactory
+     */
+    public function testDefaultFactoryCanHandleObjectWithConstructorWithSingleParameterWithoutDocBlock(): void
+    {
+        $value = random_int(0, PHP_INT_MAX);
+        $class = new class {
+            /** @var int */
+            private $value;
+
+            public function __construct(int $value = 0)
+            {
+                $this->value = $value;
+            }
+
+            public function getValue(): int
+            {
+                return $this->value;
+            }
+        };
+        $sut = new DefaultFactory(
+            get_class($class),
+            (new \ReflectionClass($class))->getConstructor(),
+            $this->createMock(JsonMapperInterface::class),
+            new ScalarCaster()
+        );
+
+        $result = $sut->__invoke((object) ['value' => $value]);
+
+        self::assertInstanceOf(get_class($class), $result);
+        self::assertEquals($value, $result->getValue());
+    }
+
+    /**
+     * @covers \JsonMapper\Middleware\Constructor\DefaultFactory
+     */
+    public function testDefaultFactoryCanHandleObjectWithConstructorWithTwoParametersWithoutDocBlock(): void
+    {
+        $first = random_int(0, PHP_INT_MAX);
+        $second = random_int(0, PHP_INT_MAX);
+        $class = new class {
+            /** @var int */
+            private $first;
+            /** @var int */
+            private $second;
+
+            public function __construct(int $first = 0, int $second = 0)
+            {
+                $this->first = $first;
+                $this->second = $second;
+            }
+
+            public function getFirst(): int
+            {
+                return $this->first;
+            }
+
+            public function getSecond(): int
+            {
+                return $this->second;
+            }
+        };
+        $sut = new DefaultFactory(
+            get_class($class),
+            (new \ReflectionClass($class))->getConstructor(),
+            $this->createMock(JsonMapperInterface::class),
+            new ScalarCaster()
+        );
+
+        $result = $sut->__invoke((object) ['second' => $second, 'first' => $first]);
+
+        self::assertInstanceOf(get_class($class), $result);
+        self::assertEquals($first, $result->getFirst());
+        self::assertEquals($second, $result->getSecond());
+    }
+
+    /**
+     * @covers \JsonMapper\Middleware\Constructor\DefaultFactory
+     */
+    public function testDefaultFactoryCanHandleObjectWithConstructorWithTwoParametersHintedThroughDocBlock(): void
+    {
+        $first = random_int(0, PHP_INT_MAX);
+        $second = random_int(0, PHP_INT_MAX);
+        $class = new class {
+            /** @var int */
+            private $first;
+            /** @var int */
+            private $second;
+
+            /**
+             * @param int $first
+             * @param int $second
+             */
+            public function __construct($first = 0, $second = 0)
+            {
+                $this->first = $first;
+                $this->second = $second;
+            }
+
+            public function getFirst(): int
+            {
+                return $this->first;
+            }
+
+            public function getSecond(): int
+            {
+                return $this->second;
+            }
+        };
+        $sut = new DefaultFactory(
+            get_class($class),
+            (new \ReflectionClass($class))->getConstructor(),
+            $this->createMock(JsonMapperInterface::class),
+            new ScalarCaster()
+        );
+
+        $result = $sut->__invoke((object) ['second' => $second, 'first' => $first]);
+
+        self::assertInstanceOf(get_class($class), $result);
+        self::assertEquals($first, $result->getFirst());
+        self::assertEquals($second, $result->getSecond());
+    }
+}
