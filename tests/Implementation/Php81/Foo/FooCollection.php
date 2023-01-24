74f84d23b56e9f35106cba1cd3c12c3c1f357bba
Danny van der Sluijs <danny.vandersluijs@icloud.com>
2023-01-24 20:39:16 +0100

    test: Add broken test from report

---
+++ tests/Implementation/Php81/Foo/FooCollection.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JsonMapper\Tests\Implementation\Php81\Foo;
+
+
+class FooCollection
+{
+    /**
+     * @param FooItem[] $items
+     */
+    public function __construct(public readonly array $items = [])
+    {
+    }
+}
---
+++ tests/Implementation/Php81/Foo/FooItem.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JsonMapper\Tests\Implementation\Php81\Foo;
+
+
+class FooItem
+{
+    /**
+     * @param BarItem[] $orders
+     */
+    public function __construct(public readonly string $name, public readonly array $orders = [],)
+    {
+    }
+}
