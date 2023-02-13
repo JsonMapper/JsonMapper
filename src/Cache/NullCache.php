3cb79a39e8fb75d58c65186236c0c96ef046739f
Danny van der Sluijs <danny.vandersluijs@icloud.com>
2023-02-13 19:59:46 +0100

    fix: Allow for psr/simple-cache 2.0 or 3.0

--- src/Cache/NullCache.php
+++ src/Cache/NullCache.php
@@ -3,53 +3,14 @@
 namespace JsonMapper\Cache;
 
 use Psr\SimpleCache\CacheInterface;
+use Symfony\Component\Cache\Adapter\NullAdapter;
+use Symfony\Component\Cache\Psr16Cache;
 
-class NullCache implements CacheInterface
+class NullCache extends Psr16Cache implements CacheInterface
 {
-    public function get($key, $default = null)
+
+    public function __construct()
     {
-        return $default;
-    }
-
-    public function set($key, $value, $ttl = null): bool
-    {
-        return true;
-    }
-
-    public function delete($key): bool
-    {
-        return true;
-    }
-
-    public function clear(): bool
-    {
-        return true;
-    }
-
-    public function getMultiple($keys, $default = null)
-    {
-        if (! \is_iterable($keys)) {
-            throw InvalidArgumentException::forCacheKey($keys);
-        }
-
-        $keys = (array) $keys;
-        $values = \array_fill(0, count($keys), $default);
-
-        return (array) \array_combine($keys, $values);
-    }
-
-    public function setMultiple($values, $ttl = null): bool
-    {
-        return true;
-    }
-
-    public function deleteMultiple($keys): bool
-    {
-        return true;
-    }
-
-    public function has($key): bool
-    {
-        return false;
+        parent::__construct(new NullAdapter());
     }
 }
